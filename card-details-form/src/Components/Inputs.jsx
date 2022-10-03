20c6229baa216bfefc28471f5bffcb858d6362e2
Ari <ariane.mrv@gmail.com>
2022-10-03 20:53:57 +0200

    update style

--- card-details-form/src/Components/Inputs.jsx
+++ card-details-form/src/Components/Inputs.jsx
@@ -6,37 +6,51 @@ export default function Inputs() {
     flex-direction: column;
     gap: 1rem;
     margin-top: 50%;
+    
   `;
   const InputName = styled.input`
     width: 380px;
     height: 45px;
     top: 24px;
     background: #dfdee0;
+    border-radius: 8px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
   `;
   const InputNumber = styled(InputName)``;
 
   const InputDate = styled.input`
     width: 80px;
     height: 45px;
-
+    border-radius: 8px;
+    
   `;
   const InputCvc = styled.input`
     width: 191px;
     height: 45px;
-    radius: 8px;
+    border-radius: 8px;
+  `;
+  const Button = styled.button`
+    width: 381px;
+    height: 53px;
+    border-radius: 8px;
+    background: #21092f;
+    color: #ffffff;
   `;
 
   return (
     <div>
       <Div>
-        <InputName />
-        <InputNumber />
+        <InputName placeholder="e.g. Ariane Crestani" />
+        <InputNumber placeholder="e.g. 1234 5678 9123 0000" />
       </Div>
       <div>
-        <InputDate />
-        <InputDate />
-        <InputCvc/>
+        <InputDate placeholder="MM" />
+        <InputDate placeholder="YY" />
+        <InputCvc placeholder="e.g. 123" />
       </div>
+      <Button>Confirm</Button>
     </div>
   );
 }

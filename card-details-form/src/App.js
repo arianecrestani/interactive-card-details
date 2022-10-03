91f42744bc33c763b0cd66975019e22bdd8cbb2f
Ari <ariane.mrv@gmail.com>
2022-10-03 11:22:01 +0200

    css style

--- card-details-form/src/App.js
+++ card-details-form/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
 
-function App() {
+export default function App() {
+  let BgMainImage = require("./images/bg-main-desktop.png");
+  let BgCardFront = require("./images/bg-card-front.png");
+  let BgCardBack = require("./images/bg-card-back.png");
+
+  const Container = styled.div`
+    display: grid;
+    grid-template-rows: 100vh;
+    grid-template-columns: repeat(3, 1fr);
+  `;
+  const Main = styled.img`
+    width: 100%;
+    height: 100%;
+    margin: 0px;
+  `;
+  const CardFront = styled.img`
+    float: left;
+    display: flex;
+    position: relative;
+    z-index: 1;
+    margin-left: -38%;
+    margin-top: 53%;
+  `;
+    const CardBack = styled.img`
+      position: relative;
+      z-index: 1;
+      margin-left: -30%;
+      margin-top: 89%;
+      display: flex;
+    `;
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div>
+        <Main alt="main" src={BgMainImage} />
+      </div>
+      <div>
+        <CardFront alt="cardFront" src={BgCardFront} />
+        <CardBack alt="cardBack" src={BgCardBack} />
+      </div>
+    </Container>
   );
 }
-
-export default App;
